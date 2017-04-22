11638ab4431b6541adeccea7d47156174ec97d21
CuongBM <cuongbm0211@gmail.com>
2017-04-22 22:17:53 +0700

    =============Step 1: ParameterizeMethod

---
+++ refactoring/src/main/java/makeMethodCallSimpler/ParameterizeMethod.java
@@ -0,0 +1,16 @@
+package makeMethodCallSimpler;
+
+/**
+ * Created by CuongBM on 4/22/2017.
+ */
+public class ParameterizeMethod {
+    double salary;
+
+    public void fivePercentRaise() {
+        salary *= 1.05;
+    }
+
+    public void tenPercentRaise() {
+        salary *= 1.1;
+    }
+}
