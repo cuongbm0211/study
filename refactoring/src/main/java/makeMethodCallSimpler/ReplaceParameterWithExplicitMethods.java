1a67f7beceaac9bd12158aa17ca700fa85d142b1
CuongBM <cuongbm0211@gmail.com>
2017-04-22 22:48:18 +0700

    =============Step 1: ReplaceParamenterWithExplicitMethods

---
+++ refactoring/src/main/java/makeMethodCallSimpler/ReplaceParameterWithExplicitMethods.java
@@ -0,0 +1,31 @@
+package makeMethodCallSimpler;
+
+/**
+ * Created by CuongBM on 4/22/2017.
+ */
+public class ReplaceParameterWithExplicitMethods {
+    public static class Employee {
+        public static final int ENGINEER = 0;
+        public static final int SALESMAN = 1;
+        public static final int MANAGER = 2;
+
+        public static Employee create(int type) {
+            switch (type) {
+                case ENGINEER:
+                    return new Engineer();
+                case SALESMAN:
+                    return new Salesman();
+                case MANAGER:
+                    return new Manager();
+            }
+            throw new RuntimeException("Unknown type: " + type);
+        }
+
+
+        public static class Engineer extends Employee {}
+
+        public static class Salesman extends Employee {}
+
+        public static class Manager extends Employee {}
+    }
+}
