c38ceb60801ea6855b61f208d53b305108f1a31d
CuongBM <cuongbm0211@gmail.com>
2017-04-23 07:01:49 +0700

    =============Step 1: PreserverWholeObject

---
+++ refactoring/src/main/java/makeMethodCallSimpler/PreserverWholeObject.java
@@ -0,0 +1,54 @@
+package makeMethodCallSimpler;
+
+/**
+ * Created by CuongBM on 4/23/2017.
+ */
+public class PreserverWholeObject {
+    static class Room {
+        private Range tempRange;
+
+        public boolean withinPlan(HeatingPlan plan) {
+            int low = tempRange.getLow();
+            int high = tempRange.getHigh();
+            return plan.isWithinRange(low, high);
+        }
+
+        public void setTempRange(Range tempRange) {
+            this.tempRange = tempRange;
+        }
+    }
+
+    static class HeatingPlan {
+        private Range tempRange;
+
+        public boolean isWithinRange(int low, int high) {
+            return (low >= tempRange.getLow() && high <= tempRange.getHigh());
+        }
+
+        public void setTempRange(Range tempRange) {
+            this.tempRange = tempRange;
+        }
+    }
+
+    static class Range {
+        private int low;
+        private int high;
+
+        public int getLow() {
+            return low;
+        }
+
+        public void setLow(int low) {
+            this.low = low;
+        }
+
+        public int getHigh() {
+            return high;
+        }
+
+        public void setHigh(int high) {
+            this.high = high;
+        }
+    }
+
+}
