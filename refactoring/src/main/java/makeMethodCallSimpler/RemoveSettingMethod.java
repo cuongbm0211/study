706f095fd54d50757a4dcc51d5de411151054ac2
CuongBM <cuongbm0211@gmail.com>
2017-05-03 23:36:14 +0700

    =============Step 1: RemoveSettingMethod

---
+++ refactoring/src/main/java/makeMethodCallSimpler/RemoveSettingMethod.java
@@ -0,0 +1,16 @@
+package makeMethodCallSimpler;
+
+/**
+ * Created by CuongBM on 5/3/2017.
+ */
+public class RemoveSettingMethod {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
