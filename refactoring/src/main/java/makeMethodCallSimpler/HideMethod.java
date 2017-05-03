5fad68c553686fed1af4cb58028689c7ca0c7902
CuongBM <cuongbm0211@gmail.com>
2017-05-03 23:50:53 +0700

    Step 2: make method private if none external class call this method

--- refactoring/src/main/java/makeMethodCallSimpler/HideMethod.java
+++ refactoring/src/main/java/makeMethodCallSimpler/HideMethod.java
@@ -4,7 +4,7 @@ package makeMethodCallSimpler;
  * Created by CuongBM on 5/3/2017.
  */
 public class HideMethod {
-    public void someMethod() {
-        
+    private void someMethod() {
+
     }
 }
