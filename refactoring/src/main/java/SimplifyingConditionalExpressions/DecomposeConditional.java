55800e3d32b2b34c12597b442d8cfc3a72f9dd2f
CuongBM <cuongbm0211@gmail.com>
2017-05-05 12:55:47 +0700

    Step 2: Extract conditional, and extract method calculateCharge

--- refactoring/src/main/java/SimplifyingConditionalExpressions/DecomposeConditional.java
+++ refactoring/src/main/java/SimplifyingConditionalExpressions/DecomposeConditional.java
@@ -6,22 +6,33 @@ import java.time.LocalDate;
  * Created by CuongBM on 5/5/2017.
  */
 public class DecomposeConditional {
-    private LocalDate currentDate;
-    private double charge;
-
     private static final LocalDate SUMMER_START = LocalDate.of(2017, 6, 1);
     private static final LocalDate SUMMER_END = LocalDate.of(2017, 9, 1);
 
+    private double charge;
+    private LocalDate currentDate;
     private int quantity;
     private double winterRate;
     private double winterServiceCharge;
     private double summerRate;
 
     public void calculateCharge() {
-        if (currentDate.isBefore(SUMMER_START) || currentDate.isAfter(SUMMER_END)) {
-            charge = quantity * winterRate + winterServiceCharge;
+        if (isNotSummer()) {
+            charge = calculateWinterCharge();
         } else {
-            charge = quantity * summerRate;
+            charge = calculateSummerCharge();
         }
+    }
+
+    private boolean isNotSummer() {
+        return currentDate.isBefore(SUMMER_START) || currentDate.isAfter(SUMMER_END);
+    }
+
+    private double calculateWinterCharge() {
+        return quantity * winterRate + winterServiceCharge;
+    }
+
+    private double calculateSummerCharge() {
+        return quantity * summerRate;
     }
 }
