766fc2540c6d8fb386896f686f7a2ec15ce4687d
CuongBM <cuongbm0211@gmail.com>
2017-05-05 15:51:16 +0700

    =============Step 1: ConsolidateDuplicateConditionalFragment

---
+++ refactoring/src/main/java/SimplifyingConditionalExpressions/ConsolidateDuplicateConditionalFragment.java
@@ -0,0 +1,23 @@
+package SimplifyingConditionalExpressions;
+
+/**
+ * Created by CuongBM on 5/5/2017.
+ */
+public class ConsolidateDuplicateConditionalFragment {
+
+    private boolean isSpecialDeal;
+    private double total;
+    private double price;
+
+    public void calculateSomething() {
+        if (isSpecialDeal) {
+            total = price * 0.95;
+            send();
+        } else {
+            total = price * 0.98;
+            send();
+        }
+    }
+
+    private void send() { }
+}
