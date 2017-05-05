c0c05ce56ceec6ed6218587d1c3e7c30810ec89a
CuongBM <cuongbm0211@gmail.com>
2017-05-05 15:52:01 +0700

    Step 2: Move duplicate code at the end

--- refactoring/src/main/java/SimplifyingConditionalExpressions/ConsolidateDuplicateConditionalFragment.java
+++ refactoring/src/main/java/SimplifyingConditionalExpressions/ConsolidateDuplicateConditionalFragment.java
@@ -12,12 +12,12 @@ public class ConsolidateDuplicateConditionalFragment {
     public void calculateSomething() {
         if (isSpecialDeal) {
             total = price * 0.95;
-            send();
         } else {
             total = price * 0.98;
-            send();
         }
+        send();
     }
 
-    private void send() { }
+    private void send() {
+    }
 }
