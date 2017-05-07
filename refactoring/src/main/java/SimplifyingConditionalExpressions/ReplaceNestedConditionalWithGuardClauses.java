32fa7a93621d77f764df80709061736ced9a1367
CuongBM <cuongbm0211@gmail.com>
2017-05-07 20:20:03 +0700

    =============Step 1: ReplaceNestedConditionalWithGuardClauses

---
+++ refactoring/src/main/java/SimplifyingConditionalExpressions/ReplaceNestedConditionalWithGuardClauses.java
@@ -0,0 +1,47 @@
+package SimplifyingConditionalExpressions;
+
+/**
+ * Created by CuongBM on 5/7/2017.
+ */
+public class ReplaceNestedConditionalWithGuardClauses {
+
+    private boolean isDead;
+    private boolean isSeparate;
+    private boolean isRetired;
+
+    // Example 1
+    public double getPayAmount() {
+        double result;
+        if (isDead) {
+            result = calculateDeadAmount();
+        } else {
+            if (isSeparate) {
+                result = calculateSeparateAmount();
+            } else {
+                if (isRetired) {
+                    result = calculateRetiredAmount();
+                } else {
+                    result = calculateNormalAmount();
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private double calculateNormalAmount() {
+        return 0;
+    }
+
+    private double calculateRetiredAmount() {
+        return 0;
+    }
+
+    private double calculateSeparateAmount() {
+        return 0;
+    }
+
+    private double calculateDeadAmount() {
+        return 0;
+    }
+}
