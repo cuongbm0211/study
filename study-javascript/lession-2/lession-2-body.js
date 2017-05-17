850e684dd0ab59a3c23f2331c610dc3ab0b5ed85
cuongbui <cuongbm0211@gmail.com>
2017-05-17 23:55:44 +0700

    Lession 2: Javascirpt tag

---
+++ study-javascript/lession-2/lession-2-body.js
@@ -0,0 +1,5 @@
+/**
+ * Created by cuongbui on 5/17/17.
+ */
+
+alert("Hello world in body tag!");
---
+++ study-javascript/lession-2/lession-2-head.js
@@ -0,0 +1,4 @@
+/**
+ * Created by cuongbui on 5/17/17.
+ */
+alert("Hello world javascript in head tag!");
