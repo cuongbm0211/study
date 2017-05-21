040a499fccbd277f7b68195bc11ea65be8491ead
cuongbui <cuongbm0211@gmail.com>
2017-05-21 15:43:01 +0700

    Lession 4: Operation in javascript

--- study-javascript/lession-4/lession-4.js
+++ study-javascript/lession-4/lession-4.js
@@ -1,3 +1,21 @@
 /**
  * Created by cuongbui on 5/21/17.
  */
+
+// Variable in java script is loosely-type variable (variable can contain anything)
+// is difference in java is strong-type variable
+
+var type = 1; // numeric variable
+console.log(type);
+
+type = 'one'; // string variable
+console.log(type);
+
+type = 1.1; // decimal variable
+console.log(type);
+
+type = true; // boolean variable
+console.log(type);
+
+type = null; // null value
+console.log(type);
---
+++ study-javascript/lession-5/lession-5.js
@@ -0,0 +1,20 @@
+/**
+ * Created by cuongbui on 5/21/17.
+ */
+
+var a = 10;
+var b = "10";
+var c = 10;
+
+// This condition only check equal value
+var test1 = a == b;
+console.log(test1);
+
+// This condition check equal value and type
+var test = a === b;
+console.log(test);
+
+// The same for: != and !==
+
+var test3 = a === c;
+console.log(test3);
