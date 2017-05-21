cc3f2465e0b385d6c381be0ea95781a7d8414832
cuongbui <cuongbm0211@gmail.com>
2017-05-21 15:08:11 +0700

    Lession 3: alert, confirm, prompt

---
+++ study-javascript/lession-3/lession-3.js
@@ -0,0 +1,28 @@
+/**
+ * Created by cuongbui on 5/21/17.
+ */
+// Step 1: alert function
+
+// // Alert text
+// alert("Hello alert");
+//
+// // alert number
+// alert(1234);
+//
+// // alert boolean
+// alert(true);
+
+
+// // Step 2: confirm function
+// var userAccept = confirm("Are you sure delete records?");
+//
+// if (userAccept) {
+//     console.log("User is accept");
+// } else {
+//     console.log("User not accept");
+// }
+
+
+// Step 3: prompt function
+var input = prompt("Please tell me your name", "no name");
+console.log(input);
