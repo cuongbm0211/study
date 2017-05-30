5a3e5eb307e31660b8e00d02910b289a79d0088c
cuongbui <cuongbm0211@gmail.com>
2017-05-30 23:23:05 +0700

    lession-1: bind event when don't use jQuery

---
+++ study-jquery/lession-1/lession-1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by cuongbui on 5/30/17.
+ */
+
+
+window.onload = function () {
+    var btnClickMe = document.getElementById('btnClickMe');
+    btnClickMe.onclick = function () {
+        console.log("Button click");
+    }
+}
+
