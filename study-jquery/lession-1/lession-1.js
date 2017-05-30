3a1faa6c230bf30fd5e58490f7c3d6fa2578ba6a
cuongbui <cuongbm0211@gmail.com>
2017-05-30 23:38:27 +0700

    lession-1: click button when have jQuery

--- study-jquery/lession-1/lession-1.js
+++ study-jquery/lession-1/lession-1.js
@@ -2,11 +2,8 @@
  * Created by cuongbui on 5/30/17.
  */
 
-
-window.onload = function () {
-    var btnClickMe = document.getElementById('btnClickMe');
-    btnClickMe.onclick = function () {
-        console.log("Button click");
-    }
-}
-
+$(document).ready(function () {
+    $('#btnClickMe').click(function () {
+        console.log("Hello jQuery");
+    });
+});
