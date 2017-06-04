2f4c3234b1386ad554006ef25160958ac7a639d6
cuongbui <cuongbm0211@gmail.com>
2017-06-04 11:03:01 +0700

    lession-9: prepend and append in jQuery

---
+++ study-jquery/lession-9/lession-9.js
@@ -0,0 +1,14 @@
+/**
+ * Created by cuongbui on 6/4/17.
+ */
+
+$(document).ready(function () {
+    $('div').prepend("Learn ");
+    // Them same div
+    // $('<span>Learn </span>').prependTo('div');
+
+    $('div').append(' tutorial');
+    // Equivalent
+    // $('<span> tutorial</span>').appendTo('div');
+});
+
