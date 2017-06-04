a3364f92fa7ce7614625debfd927fdcff71f51df
cuongbui <cuongbm0211@gmail.com>
2017-06-05 00:18:31 +0700

    lession-13: change event on jQuery

---
+++ study-jquery/lession-13/lession-13.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cuongbui on 6/4/17.
+ */
+
+$(document).ready(function () {
+    alert("aa");
+    $('input').change(function () {
+        console.log($(this).val());
+    });
+});
